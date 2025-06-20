acc5d1753f3d67b172cdada87d4f8b2265bc99e5
shaheer.p <shaheer.p@kinaracapital.com>
2025-06-21 01:20:07 +0530

    optimisation in script 4

--- lib/video.ts
+++ lib/video.ts
@@ -1,14 +1,13 @@
-
 export async function getVideoUrl(name: string) {
+  const url = `http://13.201.128.201:3001/generatevideo?name=${name}`;
   try {
-    console.log( `http://13.201.128.201:3001/generatevideo?name=${name}`,"url")
-    const response = await fetch(
-      `http://13.201.128.201:3001/generatevideo?name=${name}`
-    );
+    console.log("inside get video url");
+    const response = await fetch(url);
     const data = await response.json(); // or response.json() depending on your API
     console.log(data, "dataaaa");
-    return data
+    return data;
   } catch (error) {
+    console.log(url)
     console.error("Error calling API:", error);
   }
 }
